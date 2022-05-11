d44c99de228c1c4b737ef704cd1af1682820961d
Macmini <macmini@Mac-mini-de-Macmini.local>
2022-05-11 15:42:39 -0300

    criei arquivos para separar os exercicios

--- TerrenoUdemy.playground/Contents.swift
+++ TerrenoUdemy.playground/Contents.swift
@@ -1,29 +1,8 @@
 import UIKit
-// terreno
 
-//let largura = 10.0
-//let comprimento = 30.0
-//let valor = 200.00
-//
-//
-//let area = largura * comprimento
-//let preco = area * valor
-//
-//
-//print("Area do terreno = \(area)")
-//print("Preco do terreno = \(preco)")
+Terreno.test1()
+Terreno.test2()
 
-//let largura = 12.0
-//let comprimento = 20.0
-//let valor = 150.00
-//
-//
-//let area = largura * comprimento
-//let preco = area * valor
-//
-//
-//print("Area do terreno = \(area)")
-//print("Preco do terreno = \(preco)")
 
 //RETANGULO
 
---
+++ TerrenoUdemy.playground/Sources/Terreno.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+//terreno
+
+class Terreno {
+    
+    static func test1() {
+        
+        let largura = 10.0
+        let comprimento = 30.0
+        let valor = 200.00
+
+        let area = largura * comprimento
+        let preco = area * valor
+
+        print("Area do terreno = \(area)")
+        print("Preco do terreno = \(preco)")
+      
+    }
+    
+    static func test2() {
+        
+        let largura = 12.0
+        let comprimento = 20.0
+        let valor = 150.00
+
+        let area = largura * comprimento
+        let preco = area * valor
+
+        print("Area do terreno = \(area)")
+        print("Preco do terreno = \(preco)")
+    }
+}
+
+
