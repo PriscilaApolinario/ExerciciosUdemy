848c45faa7ab724d7e70ac82f7337e753ba89cbf
Macmini <macmini@Mac-mini-de-Macmini.local>
2022-05-11 15:11:18 -0300

    commit inicial

---
+++ TerrenoUdemy.playground/Contents.swift
@@ -0,0 +1,58 @@
+import UIKit
+// terreno
+
+//let largura = 10.0
+//let comprimento = 30.0
+//let valor = 200.00
+//
+//
+//let area = largura * comprimento
+//let preco = area * valor
+//
+//
+//print("Area do terreno = \(area)")
+//print("Preco do terreno = \(preco)")
+
+//let largura = 12.0
+//let comprimento = 20.0
+//let valor = 150.00
+//
+//
+//let area = largura * comprimento
+//let preco = area * valor
+//
+//
+//print("Area do terreno = \(area)")
+//print("Preco do terreno = \(preco)")
+
+//RETANGULO
+
+//let base = 4.0000
+//let altura = 5.0000
+//
+//
+//let area = base * altura
+//let perimetro = 2 * (base + altura)
+//let diagonal = sqrt(pow(base, 2) + pow(altura, 2))
+//
+//
+//
+//print("Area = \(String(format: "%.4f", area))")
+//print("Perimetro = \(String(format: "%.4f", perimetro))")
+//print("Diagonal = \(String(format: "%.4f", diagonal))")
+//
+
+// IDADES
+
+//let nome1 = "Maria Silva "
+//let idade1: Double = 19
+//let nome2 = "Joao Melo"
+//let idade2: Double = 20
+//let media = (idade1 + idade2) / 2
+//
+//
+//
+//print("A idade media de \(nome1)e \(nome2) é de \(media) anos")
+
+
+
