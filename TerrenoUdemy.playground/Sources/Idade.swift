187e56f3c5bdcba5af802c82261c5a740a12a92a
Macmini <macmini@Mac-mini-de-Macmini.local>
2022-05-13 14:24:52 -0300

    Adicionei arquivo

---
+++ TerrenoUdemy.playground/Sources/Idade.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+public class Idade {
+    
+    public static func test1() {
+
+        let nome1 = "Maria Silva "
+        let idade1: Double = 19
+        let nome2 = "Joao Melo"
+        let idade2: Double = 20
+        let media = (idade1 + idade2) / 2
+
+        print("A idade media de \(nome1)e \(nome2) é de \(media) anos")
+
+    }
+}
